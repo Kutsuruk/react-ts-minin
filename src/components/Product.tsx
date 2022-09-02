76e41a5d80da95e1eaf6bf4239fe6e51603a9458
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2022-09-02 15:49:14 +0300

    Created dynamic display details of product, show on btn click. Add styles depences on state.

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {IProduct} from "../models";
 
 interface ProductProps {
@@ -6,13 +6,30 @@ interface ProductProps {
 }
 
 const  Product = (props: ProductProps) => {
+    const [details, setDetails] = useState(false)
+    const btnBgClassName = details ? 'bg-red-400' : 'bg-blue-400'
+
+    const btnClasses = ['py-2 px-2 border', btnBgClassName]
+
     return(
         <div className='border py-2 px-4 rounded flex flex-col items-center mb-2'>
             <img className='w-1/6' src={props.product.image} alt={props.product.title}/>
             <p>{props.product.title}</p>
-            <span className='font-bold'>
-                {props.product.price} $
-            </span>
+            <span className='font-bold'>{props.product.price} $</span>
+
+            <button
+                className={btnClasses.join(' ')}
+                onClick={() => setDetails(prevState => !prevState)}
+            >
+                {details ? 'Hide details' : 'Show details'}
+            </button>
+
+            { details &&
+                <div className=''>
+                    <p>{ props.product.description }</p>
+                    <p>Rate: <span style={{fontWeight: 'bold'}}>{props.product.rating.rate}</span></p>
+                </div>
+            }
         </div>
     )
 }
